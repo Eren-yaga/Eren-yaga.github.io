6400ee6b8bf86854c9afb1d9b61a19407f0cd5e7
Eren-yaga <cleber.junior@estudante.ifms.edu.br>
2025-02-06 11:00:20 -0400

    Ultimo teste

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 
 <h1>Conheça nossa loja a Slifers Cell</h1>
-<p>Venha conhecer a gente <a href="/src/routes/test/+page.svelte">Slifers Cell</a> a melhor loja técnica do Brasil</p>
+<p>Venha conhecer a gente <a href="/test">Slifers Cell</a> a melhor loja técnica do Brasil</p>
 <br>
 <div class="card" style="width: 18rem;">
     <img src="/Slifer.png" class="card-img-top" alt="...">
